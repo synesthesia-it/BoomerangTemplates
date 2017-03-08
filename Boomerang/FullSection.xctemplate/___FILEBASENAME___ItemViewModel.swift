241172d3190a26e4060dd6b9270ce616a6d01beb
Fabio Dono <fabio.dono@synesthesia.it>
2017-03-08 10:06:40 +0100

    First Commit

---
+++ Boomerang/FullSection.xctemplate/___FILEBASENAME___ItemViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+import RxSwift
+import Boomerang
+
+final class ___FILEBASENAMEASIDENTIFIER___ItemViewModel : ItemViewModelType {
+    var model:ItemViewModelType.Model
+    var itemIdentifier:ListIdentifier
+    
+    init(model:ModelType) {
+        self.model = model
+    }
+}
---
+++ Boomerang/ItemWithCollectionViewCell.xctemplate/___FILEBASENAME___ItemCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CollectionViewCell.swift
+//  
+//
+//  Created by Stefano Mondino on 07/03/17.
+//
+//
+
+import UIKit
+import Boomerang
+import RxSwift
+import Action
+import RxCocoa
+
+class ___FILEBASENAMEASIDENTIFIER___ItemCollectionViewCell: UICollectionViewCell {
+    
+    var viewModel:ItemViewModelType?
+    var disposeBag = DisposeBag()
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    func bind(_ viewModel: ViewModelType?) {
+        guard let viewModel = viewModel as? ___FILEBASENAMEASIDENTIFIER___ItemViewModel else {
+            return
+        }
+        self.viewModel = viewModel
+    }
+}
