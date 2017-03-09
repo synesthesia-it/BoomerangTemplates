c25f06e31629fe337638fe1d3e525cfe5ef0bf8f
Fabio Dono <fabio.dono@synesthesia.it>
2017-03-09 11:07:16 +0100

    nuova versione installer

--- installBoomerangTemplates.sh
+++ installBoomerangTemplates.sh
@@ -4,11 +4,12 @@
 REPONAME="BoomerangTemplates"
 BOOMERANGDIRNAME="Boomerang"
 TEMPLATEDIRECTORY="$HOME/Library/Developer/Xcode/Templates/File Templates/"
+TEMPLATEDIRCREATE = "$HOME/Library/Developer/Xcode/Templates/Filei\ Templates/"
 
 function installTemplates {
 	rm -rf "$TEMPLATEDIRECTORY$BOOMERANGDIRNAME"
 
-	mkdir -p "$TEMPLATEDIRECTORY"
+	mkdir -p "$TEMPLATEDIRCREATE"
 	cd $REPONAME
 	cp -r "$BOOMERANGDIRNAME" "$TEMPLATEDIRECTORY"
 	cd ..
