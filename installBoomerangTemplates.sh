26a61a0754ff804aebc1f3a27a24e7995d92ff9f
Fabio Dono <fabio.dono@synesthesia.it>
2017-03-09 10:00:23 +0100

    modifica  install script

--- installBoomerangTemplates.sh
+++ installBoomerangTemplates.sh
@@ -5,11 +5,14 @@ REPONAME="BoomerangTemplates"
 BOOMERANGDIRNAME="Boomerang"
 TEMPLATEDIRECTORY="$HOME/Library/Developer/Xcode/Templates/File Templates/"
 
-rm -rf "$TEMPLATEDIRECTORY$BOOMERANGDIRNAME"
+function install {
+	rm -rf "$TEMPLATEDIRECTORY$BOOMERANGDIRNAME"
 
-git clone https://github.com/synesthesia-it/BoomerangTemplates.git
-cd $REPONAME
-cp -r "$BOOMERANGDIRNAME" "$TEMPLATEDIRECTORY"
-cd ..
-rm -rf $REPONAME
-echo "Boomerang templates installed"
+	cd $REPONAME
+	cp -r "$BOOMERANGDIRNAME" "$TEMPLATEDIRECTORY"
+	cd ..
+	rm -rf $REPONAME
+	echo "Boomerang templates installed"
+}
+
+git clone https://github.com/synesthesia-it/BoomerangTemplates.git && install
