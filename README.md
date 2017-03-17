2338864154e8c30f4e91f403751f92eb5a3e951c
Stefano Mondino <stefano.mondino.dev@gmail.com>
2017-03-17 18:04:06 +0100

    project template split

--- README.md
+++ README.md
@@ -3,5 +3,11 @@
 in order to install Boomerang templates in your Xcode folder, just copypaste the command below in your terminal.
 
 ```bash
-curl -fsSL https://raw.githubusercontent.com/synesthesia-it/BoomerangTemplates/master/installBoomerangTemplates.sh | sh
+curl -fsSL https://raw.githubusercontent.com/synesthesia-it/BoomerangTemplates/master/installBoomerangFileTemplates.sh | sh
+```
+
+
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/synesthesia-it/BoomerangTemplates/master/installBoomerangProjectTemplate.sh | sh
 ```
---
+++ installBoomerangProjectTemplate.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+#
+REPONAME="BoomerangTemplates"
+BOOMERANGDIRNAME="Boomerang"
+TEMPLATEDIRECTORY="$HOME/Library/Developer/Xcode/Templates/Project Templates/"
+
+function installTemplates {
+	rm -rf "$TEMPLATEDIRECTORY$BOOMERANGDIRNAME"
+
+	cd $REPONAME
+	cp -r "ProjectTemplate" "$TEMPLATEDIRECTORY"
+	cd ..
+	rm -rf $REPONAME
+	echo "Boomerang templates installed"
+}
+
+mkdir -p $HOME/Library/Developer/Xcode/Templates/Project\ Templates
+cd /tmp/
+rm -rf BoomerangTemplates
+git clone https://github.com/synesthesia-it/BoomerangTemplates.git && installTemplates
