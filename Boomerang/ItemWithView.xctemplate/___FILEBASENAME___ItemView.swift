ec9993a236cb540e317c6479a467fc3fbfd61620
Stefano Mondino <stefano@Mac-mini-di-Stefano.local>
2017-06-25 22:51:51 +0200

    embeddable views

---
+++ Boomerang/ItemWithView.xctemplate/___FILEBASENAME___ItemView.swift
@@ -0,0 +1,29 @@
+//
+//  CollectionViewCell.swift
+//  
+//
+//  Created by Stefano Mondino on 07/03/17.
+//
+//
+
+import UIKit
+import Boomerang
+import RxSwift
+import Action
+import RxCocoa
+
+class ___FILEBASENAMEASIDENTIFIER___ItemView: UIView, ViewModelBindable, EmbeddableView {
+    
+    var viewModel:ItemViewModelType?
+    var disposeBag = DisposeBag()
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    func bind(to viewModel: ViewModelType?) {
+        guard let viewModel = viewModel as? ___FILEBASENAMEASIDENTIFIER___ItemViewModel else {
+            return
+        }
+        self.viewModel = viewModel
+    }
+}
